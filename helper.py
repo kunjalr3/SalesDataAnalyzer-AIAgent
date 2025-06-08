fca6c4e873df2bff2576671ac0f5bf570c549761
Kunjal Rathod <kunjalrathod@gmail.com>
2025-06-08 16:45:55 -0700

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,13 @@
+# Add your utilities or helper functions to this file.
+
+import os
+from dotenv import load_dotenv, find_dotenv
+                     
+def load_env():
+    _ = load_dotenv(find_dotenv(), override=True)
+
+def get_openai_api_key():
+    load_env()
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    return openai_api_key
+
